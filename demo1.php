905bc522d808c73813a16b7ec69ec32dffdc503e
thanhthu98 <thanhthu18298@gmail.com>
2018-11-16 22:15:39 +0700

    The first upload

---
+++ demo1.php
@@ -0,0 +1,20 @@
+<!Doctype <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <?php
+        echo '<h1>Hello world</h1>';
+        echo '<br><h2>Note:</h2>';
+        echo '<br>Đường dẫn lưu file php để test = C:\Apache24\htdocs\TEST PHP';
+        echo '<br>Mở trình duyệt với link: localhost';
+        echo '<br>The end...';
+    ?>
+</body>
+</html>
---
+++ vd.php
@@ -0,0 +1,33 @@
+<!Doctype <!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Page Title</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" type="text/css" media="screen" href="vd.css" />
+    <script src="main.js"></script>
+</head>
+<body>
+    <p>Hello</p>
+    <div id="Footer">Bye</div>
+    <table border="1" width="300px">
+        <tr>
+            <td colspan="2" rowspan="2">data</td>
+            <td>data</td>
+            <td>data</td>
+        </tr>
+        <tr>
+            <td>data</td>
+            <td>data</td>
+        </tr>
+        <tr>
+            <td>data</td>
+            <td colspan="3" rowspan=2>data</td>
+        </tr>
+        <tr>
+            <td>data</td>
+        </tr>
+    </table>
+</body>
+</html>
